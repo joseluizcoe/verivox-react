cd3c08ae22a319817265700fab59d03cfa8101ef
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-03 01:03:48 -0200

    Adding reference to offerList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { OfferList } from './components/OfferList';
+import offers from './services/offers';
 
 function App() {
+  
   return (
-    <div className="App">
-
+    <div className='App'>
+      <OfferList offers={offers} />
     </div>
   );
 }
