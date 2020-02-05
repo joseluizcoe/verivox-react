1c28e68b6f74b299ffe4da46ce7d221643422160
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-05 03:12:05 -0200

    Creating a generic handleChange to be used by generic fields via select box.

--- src/App.js
+++ src/App.js
@@ -43,16 +43,21 @@ class App extends Component {
   };
 
   
-  handleDownloadSpeed = () => {
+  handleChange = async (event) => {
+    let selectedIndex = event.target.selectedIndex;
+    let splitedValues = event.target[selectedIndex].value.split(',');
+    let [field, order] = splitedValues;
+
     let newState = {
       sort: {
-        field: 'downloadSpeed',
-        order: 'asc'
+        field,
+        order
       }
     };
-    this.setState(newState);
 
+    await this.setState(newState);
     this.changeOrder(this.genericSort);
+
   };
 
   render() {
@@ -60,13 +65,19 @@ class App extends Component {
 
     return (
       <div className='App'>
-        {loading ? (
+        { loading ? (
           'Loading'
         ) : (
           <>
-            <button onClick={this.handleDownloadSpeed}>
-              Download
-            </button>
+            <select onChange={this.handleChange}>
+              <option value="rank,asc">Order by:</option>
+              <option value='downloadSpeed,asc'>Download Speed - Asc</option>
+              <option value='downloadSpeed,desc'>Download Speed - Desc</option>
+              <option value='uploadSpeed,asc'>Upload Speed - Asc</option>
+              <option value='uploadSpeed,desc'>Upload Speed - Desc</option>
+              <option value='price,asc'>Price - Asc</option>
+              <option value='price,desc'>Price - Desc</option>
+            </select>
             <OfferList offers={this.state.sortedOffers} />
           </>
         )}
