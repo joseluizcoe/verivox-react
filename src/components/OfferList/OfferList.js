cacb2015cfe64d8e350584d21b5f4fddf8993cb1
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-05 03:33:03 -0200

    Consuming data from AppContext.

--- src/components/OfferList/OfferList.js
+++ src/components/OfferList/OfferList.js
@@ -3,17 +3,20 @@ import { OfferItem } from '../OfferItem';
 import OfferListStyled from './OfferListStyles';
 import {SortOfferBar} from '../SortOfferBar';
 
-const OfferList = props => (
-  <OfferListStyled>
-    <SortOfferBar />
-    { props.offers &&
-      props.offers.map(
-        (offer, index) => (
-          <OfferItem offer={offer} />
-        )
-      )
-    }
-  </OfferListStyled>
+import AppContext from '../../context';
+
+const OfferList = () => (
+  <AppContext.Consumer>
+    {context => (
+      <OfferListStyled>
+        <SortOfferBar />
+        {context.state.offers &&
+          context.state.offers.map((offer, index) => (
+            <OfferItem offer={offer} />
+          ))}
+      </OfferListStyled>
+    )}
+  </AppContext.Consumer>
 );
 
 export default OfferList;
