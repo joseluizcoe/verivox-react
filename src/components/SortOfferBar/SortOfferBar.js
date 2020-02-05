c37dbc7cb6ce1c137e70c6514fe6478c04bbe393
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-05 04:07:26 -0200

    Styling sort offer bar

--- src/components/SortOfferBar/SortOfferBar.js
+++ src/components/SortOfferBar/SortOfferBar.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import AppContext from '../../context';
+import SortOfferBarStyled from './SortOfferBarStyled';
 
 const SortOfferBar = () => (
   <AppContext.Consumer>
     {context => (
-      <select onChange={context.handleChange}>
-        <option value='rank,asc'>Order by:</option>
-        <option value='downloadSpeed,asc'>
-          Download Speed - Asc
-        </option>
-        <option value='downloadSpeed,desc'>
-          Download Speed - Desc
-        </option>
-        <option value='uploadSpeed,asc'>
-          Upload Speed - Asc
-        </option>
-        <option value='uploadSpeed,desc'>
-          Upload Speed - Desc
-        </option>
-        <option value='price,asc'>Price - Asc</option>
-        <option value='price,desc'>Price - Desc</option>
-      </select>
+      <SortOfferBarStyled>
+        <h2>
+          {context.state.offers.totalResults} Results.
+        </h2>
+        <select onChange={context.handleChange}>
+          <option value='rank,asc'>Order by:</option>
+          <option value='downloadSpeed,asc'>
+            Download Speed - Asc
+          </option>
+          <option value='downloadSpeed,desc'>
+            Download Speed - Desc
+          </option>
+          <option value='uploadSpeed,asc'>
+            Upload Speed - Asc
+          </option>
+          <option value='uploadSpeed,desc'>
+            Upload Speed - Desc
+          </option>
+          <option value='price,asc'>Price - Asc</option>
+          <option value='price,desc'>Price - Desc</option>
+        </select>
+      </SortOfferBarStyled>
     )}
   </AppContext.Consumer>
 );
