4f685bc61915c05be0dce2ce34e509d36c05395c
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-05 03:44:06 -0200

    Getting handleChange function from context.

--- src/components/SortOfferBar/SortOfferBar.js
+++ src/components/SortOfferBar/SortOfferBar.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import AppContext from '../../context';
 
-const SortOfferBar = props => (
-  <select onChange="">
-    <option value='rank,asc'>Order by:</option>
-    <option value='downloadSpeed,asc'>
-      Download Speed - Asc
-    </option>
-    <option value='downloadSpeed,desc'>
-      Download Speed - Desc
-    </option>
-    <option value='uploadSpeed,asc'>
-      Upload Speed - Asc
-    </option>
-    <option value='uploadSpeed,desc'>
-      Upload Speed - Desc
-    </option>
-    <option value='price,asc'>Price - Asc</option>
-    <option value='price,desc'>Price - Desc</option>
-  </select>
+const SortOfferBar = () => (
+  <AppContext.Consumer>
+    {context => (
+      <select onChange={context.handleChange}>
+        <option value='rank,asc'>Order by:</option>
+        <option value='downloadSpeed,asc'>
+          Download Speed - Asc
+        </option>
+        <option value='downloadSpeed,desc'>
+          Download Speed - Desc
+        </option>
+        <option value='uploadSpeed,asc'>
+          Upload Speed - Asc
+        </option>
+        <option value='uploadSpeed,desc'>
+          Upload Speed - Desc
+        </option>
+        <option value='price,asc'>Price - Asc</option>
+        <option value='price,desc'>Price - Desc</option>
+      </select>
+    )}
+  </AppContext.Consumer>
 );
 
 export default SortOfferBar;
