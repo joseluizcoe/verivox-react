d41e80a66feaf615c7ff4c20aec598abd4818b8a
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-05 02:17:26 -0200

    Returning only used fields.

--- src/services/offers.js
+++ src/services/offers.js
@@ -1,3 +1,27 @@
 import * as offers from './offers.json';
 
-export default offers.offers;
+const allOffers = offers.offers.map(
+  offer => {
+    return {
+      rank: offer.rank,
+      providerLogoUrl: offer.provider.logoUrl,
+      providerDescription: offer.provider.content.text,
+      productTitle: offer.product.content.text,
+      downloadSpeed:
+      offer.contractTerm.downloadSpeed.amount,
+      downloadSpeedUnit:
+      offer.contractTerm.downloadSpeed.unit,
+      uploadSpeed: offer.contractTerm.uploadSpeed.amount,
+      uploadSpeedUnit: offer.contractTerm.uploadSpeed.unit,
+      remarks: offer.remarks,
+      price: offer.cost.amount,
+      priceText: offer.cost.effectiveCost.content.text,
+      hasSignupPage: offer.signup.responsive,
+      signupPageUrl:
+      offer.signup.responsive ? offer.signup.responsive.url : null
+    };
+  }
+);
+
+
+export default allOffers;
