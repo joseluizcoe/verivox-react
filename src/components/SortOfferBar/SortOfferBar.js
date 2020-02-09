def26adc84dc24434a0df1ab3eb23be391d9c7c4
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-09 00:23:48 -0200

    Refactoring "order by" options

--- src/components/SortOfferBar/SortOfferBar.js
+++ src/components/SortOfferBar/SortOfferBar.js
@@ -1,37 +1,7 @@
 import React from 'react';
 import AppContext from '../../context';
 import SortOfferBarStyled from './SortOfferBarStyled';
-
-const ORDER_BY_OPTIONS = [
-  {
-    title: 'Order by:',
-    value: 'rank,asc'
-  },
-  {
-    title: 'Download Speed - Asc',
-    value: 'downloadSpeed,asc'
-  },
-  {
-    title: 'Download Speed - Desc',
-    value: 'downloadSpeed,desc'
-  },
-  {
-    title: 'Upload Speed - Asc',
-    value: 'uploadSpeed,asc'
-  },
-  {
-    title: 'Upload Speed - Desc',
-    value: 'uploadSpeed,desc'
-  },
-  {
-    title: 'Price - Asc',
-    value: 'price,asc'
-  },
-  {
-    title: 'Price - Desc',
-    value: 'price,desc'
-  }
-];
+import ORDER_BY_OPTIONS from './constants';
 
 const SortOfferBar = () => (
   <AppContext.Consumer>
---
+++ src/components/SortOfferBar/constants.js
@@ -0,0 +1,33 @@
+export const ORDER_BY_OPTIONS = [
+  {
+    title: 'Order by:',
+    value: 'rank,asc'
+  },
+  {
+    title: 'Download Speed - Asc',
+    value: 'downloadSpeed,asc'
+  },
+  {
+    title: 'Download Speed - Desc',
+    value: 'downloadSpeed,desc'
+  },
+  {
+    title: 'Upload Speed - Asc',
+    value: 'uploadSpeed,asc'
+  },
+  {
+    title: 'Upload Speed - Desc',
+    value: 'uploadSpeed,desc'
+  },
+  {
+    title: 'Price - Asc',
+    value: 'price,asc'
+  },
+  {
+    title: 'Price - Desc',
+    value: 'price,desc'
+  }
+];
+
+
+export default ORDER_BY_OPTIONS;
