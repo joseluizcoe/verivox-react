b6ec2f3cb45e246e56f3f819ae5c73d10c20dafb
Jose Luiz Coe <joseluizcoe@gmail.com>
2020-02-09 00:57:34 -0200

    Converting component to use hook 'useContext'

--- src/components/SortOfferBar/SortOfferBar.js
+++ src/components/SortOfferBar/SortOfferBar.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AppContext from '../../context';
 import SortOfferBarStyled from './SortOfferBarStyled';
 import ORDER_BY_OPTIONS from './constants';
 
-const SortOfferBar = () => (
-  <AppContext.Consumer>
-    {context => (
-      <SortOfferBarStyled>
-        <h2>
-          {context.state.offers.totalResults} Results.
-        </h2>
-        <select onChange={context.handleChange}>
-          {
-            ORDER_BY_OPTIONS.map(
-              ({title, value}) => (
-                <option value={value}>{title}</option>
-              )
+const SortOfferBar = () => {
+  const { state, handleChange } = useContext(AppContext);
+  return (
+    <SortOfferBarStyled>
+      <h2>
+        {state.offers.totalResults} Results.
+      </h2>
+      <select onChange={handleChange}>
+        {
+          ORDER_BY_OPTIONS.map(
+            ({ title, value }) => (
+              <option value={value}>{title}</option>
             )
-          }
-        </select>
-      </SortOfferBarStyled>
-    )}
-  </AppContext.Consumer>
-);
+          )
+        }
+      </select>
+    </SortOfferBarStyled>
+  );
+}
 
 export default SortOfferBar;
